07b89bbeec09c3cb74bfa522c0679a15df4b11f7
sourabhpande532 <sourabhpande43@gmail.com>
2026-01-31 13:05:16 +0530

    Feat: Add option of login as guest

--- client/src/component/log/Form.jsx
+++ client/src/component/log/Form.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import "./login.css";
-// import ""
 import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+
 const Form = () => {
   const [form, setForm] = useState({ email: "", password: "" });
   const { login } = useAuth();
@@ -15,9 +15,22 @@ const Form = () => {
       navigate("/", { replace: true });
     }
   };
+  const handleGuestLogin = async () => {
+    const guestCredentials = {
+      email: "a@gmail.com",
+      password: "5321",
+    };
+    setForm(guestCredentials); // auto-fill inputs
+    const success = await login(guestCredentials);
+    if (success) {
+      navigate("/", { replace: true });
+    }
+  };
+
   return (
     <div className='login-box'>
       <h2>Login</h2>
+
       <form onSubmit={handleLogin}>
         <div className='user-box'>
           <input
@@ -26,8 +39,9 @@ const Form = () => {
             onChange={(e) => setForm({ ...form, email: e.target.value })}
             required
           />
-          <label>Username</label>
+          <label>Email</label>
         </div>
+
         <div className='user-box'>
           <input
             type='password'
@@ -37,15 +51,16 @@ const Form = () => {
           />
           <label>Password</label>
         </div>
-        <button type='submit'>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+
+        <button type='submit' className='login-btn'>
           Submit
         </button>
-        <br></br>
-        <br></br>
+
+        <button type='button' className='guest-btn' onClick={handleGuestLogin}>
+          Login as Guest
+        </button>
+        <br />
+        <br />
         <Link className='sign' to='/signup'>
           Sign Up
         </Link>
@@ -53,4 +68,5 @@ const Form = () => {
     </div>
   );
 };
+
 export { Form };
